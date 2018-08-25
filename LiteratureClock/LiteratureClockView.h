9584cbe09dc5c9ed589250dbcfba393cd04babd9
Sash Zats <sash@zats.io>
2018-08-25 12:31:02 -0400

    Initial Commit

---
+++ LiteratureClock/LiteratureClockView.h
@@ -0,0 +1,13 @@
+//
+//  LiteratureClockView.h
+//  LiteratureClock
+//
+//  Created by Sash Zats on 8/25/18.
+//  Copyright © 2018 Sash Zats. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface LiteratureClockView : ScreenSaverView
+
+@end
---
+++ LiteratureClock/LiteratureClockView.m
@@ -0,0 +1,52 @@
+//
+//  LiteratureClockView.m
+//  LiteratureClock
+//
+//  Created by Sash Zats on 8/25/18.
+//  Copyright © 2018 Sash Zats. All rights reserved.
+//
+
+#import "LiteratureClockView.h"
+
+@implementation LiteratureClockView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
